c6cccdbe5d88c7859cf8b2fa26f5e6822e2bc5cb
Younes Abdessalem <younesabdessalem31@gmail.com>
2023-02-28 15:01:51 +0100

    Amers et trajectoire suivant x

--- projet_bloc3.py
+++ projet_bloc3.py
@@ -1,6 +1,11 @@
 ## Projet BLOC 3
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+
+
+
+# Plutot definir une loi avec xk+1, fk+1, uk pour le robot
 
 
 ## Definir les amers
@@ -40,14 +45,43 @@ def amers(Nb_amer : int, x0 : float, y0 : float) :
         
     xa = m[:,0]
     ya = m[:,1]
+    
     return (xa,ya)
 
-#def trajectoire(xr : float, yr : float, thetar : float) :
+
+#definir le robot
+def robot(r : float, u : float):
+    r = r + u
+    return (r)
 
 
+## Initialisation
+x_r = 0
+y_r = 0
+theta_r = 0
+t=0
+r = np.array([[x_r, y_r, theta_r]])
+r1 = r
+
+## Calcul des positions du robot
+while(t<10):
+    u = np.array([[1 , 0 , 0]])
+    r1 = robot(r1,u)
+    r = np.concatenate((r, r1), axis = 0)
+    #plt.scatter(r[t,0],r[t,1])
+    t=t+1
+print(r)
 
 
+#Affichage
+t = 0
+fig, ax = plt.subplots()
+while(t<10):
+    
 ## Affichage de la map
-(xa,ya)=amers(8,0,1)
-plt.scatter(xa,ya)
-plt.show()
+    (xa,ya)=amers(8,0,1)    # 8 amers, position du premier a (0,1)
+    ax.scatter(xa,ya)
+
+#Affichage de la trajectoire du robot dans la map
+    ax.plot(r[t,0], r[t,1])
+    plt.show()
